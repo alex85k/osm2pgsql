c023e2970e87a2afd926caa405071a44a317b26c
Alexei Kasatkin <alexei_kasatkin@mail.ru>
2014-07-28 00:19:44 +0600

    Add PBF support on Windows (needs protobuf-c)

--- build-cmake/build.bat
+++ build-cmake/build.bat
@@ -1,3 +1,6 @@
 set CMAKE_PREFIX_PATH=C:\PROJ;x:\libs18;C:\Program Files\PostgreSQL\9.3
 set BZIPOPTS=-DBZIP2_INCLUDE_DIR=x:\libs18\include -DBZIP2_LIBRARY_RELEASE=x:\libs18\lib\libbz2.lib
-cmake .. -G "Visual Studio 12 Win64" -DCMAKE_BUILD_TYPE=Release %BZIPOPTS%
+
+call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
+cmake .. -G "NMake Makefiles" -DBUILD_PBF=ON -DCMAKE_BUILD_TYPE=Release %BZIPOPTS% 
+nmake
